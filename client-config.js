78706f4dcfe423104d680b068ae1c71cd6a84597
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-09-28 12:07:45 +0200

    Update configuration

--- client-config.js
+++ client-config.js
@@ -1,7 +1,8 @@
 export const config = {
     apiUrl: null,
     wsUrl: null,
-    githubClientId: "6b798671b021e690e7d2"
+    githubClientId: "6b798671b021e690e7d2",
+    googleAnalytics: "UA-157555549-4"
 };
 
 export default {
